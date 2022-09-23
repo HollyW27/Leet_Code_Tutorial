9b7faefabc9e598ccf0d54206eb4f2eb8d142809
HollyW27 <111414391+HollyW27@users.noreply.github.com>
2022-09-23 20:23:07 +0100

    Reverses only the letters in a string

---
+++ LeetCodeSolutions/Reverse_Only_Letters.cs
@@ -0,0 +1,31 @@
+﻿namespace ReverseOnlyLetters;
+
+public class Solution 
+{
+    public string ReverseOnlyLetters(string s) 
+    {
+        StringBuilder modified = new StringBuilder(s);
+        int j = s.Length - 1;
+        int i = 0;
+        while(i < j)
+        {
+            if (!Char.IsLower(modified[i]) && !Char.IsUpper(modified[i]))
+            {
+                i++;
+            }
+            else if (!Char.IsLower(modified[j]) && !Char.IsUpper(modified[j]))
+            {
+                j--;
+            }
+            else
+            {
+                modified[i] = modified[j];
+                modified[j] = s[i];
+                j--;
+                i++;
+            }
+        }
+        string a = modified.ToString();
+        return a;
+    }
+}
