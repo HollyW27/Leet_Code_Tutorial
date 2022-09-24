85e06947e8cf6e6a9c3deeaadd6a3c0648be8cf4
HollyW27 <111414391+HollyW27@users.noreply.github.com>
2022-09-24 18:39:53 +0100

    Checks the minimum amount of moves required in a game

---
+++ LeetCodeSolutions/MinMoves.cs
@@ -0,0 +1,18 @@
+﻿namespace MinMoves;
+
+public class Solution 
+    {
+        public int MinimumMoves(string s) 
+        {
+            int moves = 0;
+            for(int i = 0; i < s.Length; i++)
+            {
+                if(s[i] == 'X')
+                {
+                    moves++;
+                    i = i + 2;
+                }
+            }
+            return moves;
+        }
+    }
