1df486e64dc0dd485bf30a561c34a61077ff29fb
HollyW27 <111414391+HollyW27@users.noreply.github.com>
2022-09-29 20:16:21 +0100

    Finds the minimum sum of a 4 digit integer broken into two, 2 digit integers

---
+++ LeetCodeSolutions/MinimumSum.cs
@@ -0,0 +1,22 @@
+﻿namespace MinimumSum;
+
+public class Solution 
+    {
+        public int MinimumSum(int num) 
+        {
+            int newAns1 = 0;
+            int[] numbers = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                numbers[i] = num % 10;
+                num = num / 10;
+            }
+            Array.Sort(numbers);
+            newAns1 = numbers[0] + numbers[1];
+            newAns1 = newAns1 * 10;
+            newAns1 = newAns1 + numbers[2] + numbers[3];
+
+            return newAns1;
+
+        }
+    }
