b7b1f8a60b0005da4be115021ef5cf9ee52c3754
HollyW27 <111414391+HollyW27@users.noreply.github.com>
2022-09-29 20:16:42 +0100

    Checks if 2 rectangles overlap

---
+++ LeetCodeSolutions/RectangleOverlap.cs
@@ -0,0 +1,25 @@
+﻿namespace RectangleOverlap;
+
+public class Solution 
+{
+    public bool IsRectangleOverlap(int[] rec1, int[] rec2) 
+    { 
+        bool checkX = false;
+        bool checkY = false;
+        if(rec1[2] > rec2[0])
+        {
+            if(rec1[0] < rec2[2])
+            {
+                checkX = true;
+            }
+        }
+        if(rec1[3] > rec2[1])
+        {
+            if(rec1[1] < rec2[3])
+            {
+                checkY = true;
+            }
+        }
+        return checkX && checkY;
+    }
+}
