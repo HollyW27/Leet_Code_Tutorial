1e51ace337b1f965c6eef27be31c05d2ce2ac98e
HollyW27 <111414391+HollyW27@users.noreply.github.com>
2022-09-29 20:15:00 +0100

    Counts the amount of common words in two strings

---
+++ LeetCodeSolutions/Count_Common_Words.cs
@@ -0,0 +1,24 @@
+﻿namespace CountCommonWords;
+
+public class Solution 
+{
+    int answer = 0;
+    int words1Count = 0;
+    int words2Count = 0;
+    public int CountWords(string[] words1, string[] words2) 
+    {
+        for(int i = 0; i < words1.Length; i++)
+        {
+            words1Count = words1.Count(x => (x == words1[i]));
+            if(words1Count == 1)
+            {
+                words2Count = words2.Count(x => (x == words1[i]));
+                if(words2Count == 1)
+                {
+                    answer += 1;
+                }
+            }
+        }
+        return answer;
+    }
+}
